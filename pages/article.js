80c2c9beb39c46782bbe8af3d7788649703fa67b
System Administrator <root@Sachins-MacBook-Air.local>
2019-05-05 01:55:29 +0530

    first version

---
+++ pages/article.js
@@ -0,0 +1,129 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Article = () => (
+  <div>
+    <Head>
+        <title>The Lorem and Ipsum of Styling Pancakes - FB TELUGU</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="theme-color" content="#000000" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta property="og:site_name" content="webcam.fbtelugu.com" />
+        <meta property="og:image" content="https://s3-us-west-2.amazonaws.com/s.cdpn.io/8533/pancakes.jpg" name="image" />
+        <meta property="og:image:secure_url" content="https://s3-us-west-2.amazonaws.com/s.cdpn.io/8533/pancakes.jpg" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="500" />
+        <meta property="og:image:alt" content="FB Telugu" />
+        <meta property="og:title" content="The Lorem and Ipsum of Styling Pancakes - FB TELUGU" name="title" />
+        <meta content="news, trending, latest news" name="keywords" />
+        <meta property="og:description" content="A pancake is a flat cake, often thin, and round, prepared from a starch-based batter that may also contain eggs, milk and butter and cooked on a hot surface such as a griddle or frying pan, often with oil or butter. In Britain, pancakes are often unleavened,
+            and resemble a crêpe. In North America, a raising agent is used (typically baking powder). The North American pancake is similar to a Scotch pancake or drop scone. " name="description" />
+        <meta content="A pancake is a flat cake, often thin, and round, prepared from a starch-based batter that may also contain eggs, milk and butter and cooked on a hot surface such as a griddle or frying pan, often with oil or butter. In Britain, pancakes are often unleavened,
+            and resemble a crêpe. In North America, a raising agent is used (typically baking powder). The North American pancake is similar to a Scotch pancake or drop scone. " name="description" />
+        <meta property="og:url" content="https://webcam.fbtelugu.com/article" />
+        <meta property="og:type" content="website" />
+    </Head>
+    <div className="holder">
+    <article className="article">
+        <header className="header">
+            <div className="category">Life</div>
+            <h1 className="headline">
+            The Lorem and Ipsum of Stylin&rsquo; Pancakes
+            </h1>
+            <div className="meta">
+            <span className="meta-border"></span>
+            <span className="author">by <span className="author-avatar"><img src="http://gravatar.com/avatar/6fafd1e5977a3d9fdccb7207bb1175ba" /></span><span className="author-name">Jimmy Wafflehoff</span></span>
+            <span className="pub-date">Posted Sept. 22, 2015</span>
+            </div>
+        </header>
+
+        <p>A pancake is a flat cake, often thin, and round, prepared from a starch-based batter that may also contain eggs, milk and butter and cooked on a hot surface such as a griddle or frying pan, often with oil or butter. In Britain, pancakes are often unleavened,
+            and resemble a crêpe. In North America, a raising agent is used (typically baking powder). The North American pancake is similar to a Scotch pancake or drop scone. Commercially prepared pancake mixes are produced in some countries.</p>
+        
+        <p>They may be served at any time with a variety of toppings or fillings including jam, fruit, syrup, chocolate chips, or meat. In America, they are typically considered to be a breakfast food. In Britain and the Commonwealth, they are associated with
+            Shrove Tuesday, commonly known as Pancake Day, when perishable ingredients had to be used up before the fasting period of Lent began.</p>
+        
+        <p>Archaeological evidence suggests that pancakes are probably the earliest and most widespread cereal food eaten in prehistoric societies.<sup>[1]</sup> The pancake's shape and structure varies worldwide. A crêpe is a thin Breton pancake cooked on one or both sides
+            in a special pan or crepe maker to achieve a lacelike network of fine bubbles. A well-known variation originating in Southeast Europe is Palačinke, a thin moist pancake fried on both sides and filled with jam, cheese cream, chocolate, or ground walnuts,
+            but many other fillings, both sweet or savory, can also be used.</p>
+        
+        <figure>
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/8533/pancakes.jpg" alt="American style pancakes" />
+            <figcaption><strong>Figure 1.</strong> American style pancakes.</figcaption>
+        </figure>
+        <h2>History</h2>
+        <p>
+            The Ancient Greeks made pancakes called τηγανίτης (tēganitēs), ταγηνίτης (tagēnitēs)<sup>[2]</sup> or ταγηνίας (tagēnias),<sup>[3]</sup> all words deriving from τάγηνον (tagēnon), "frying pan".<sup>[4]</sup> The earliest attested references on tagenias are in the works of the 5th century BC poets Cratinus<sup>[5]</sup> and Magnes.<sup>[6]</sup> Tagenites were made with wheat flour, olive oil, honey, and curdled milk, and were served for breakfast.
+        </p>
+        <h2>Regional varieties</h2>
+        <h3>Africa</h3>
+        <p>Lahoh is a pancake-like bread originating in Somalia, Djibouti and Yemen.[18][19] It is often eaten along with honey, ghee and tea. During lunch, lahoh is sometimes consumed with curry, soup or stew.</p>
+        <h3>Europe</h3>
+        <p>Eastern-Slavic cuisines have a long tradition of pancake cooking and include a large variety of pancake types. In Belarus, Russia and Ukraine, pancakes may be breakfast food, appetizer, main course, or even dessert.</p>
+        <figure>
+            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/8533/nalesniki.jpg" alt="Polish nalesniki" />
+            <figcaption><strong>Figure 2.</strong> Polish style stuffed crepes.</figcaption>
+        </figure>
+        <h3>Asia</h3>
+        <p>Banana pancakes are a menu item in Western-oriented backpackers' cafes in Asian countries such as Thailand, Cambodia, Vietnam, India, and China. This has elicited the term Banana Pancake Trail.</p>
+        <h3>Americas</h3>
+        <p>American and Canadian pancakes (sometimes called hotcakes, griddlecakes, or flapjacks) are usually served at breakfast, in a stack of two or three pancakes topped with real or artificial maple syrup and butter, and often served with sides such as bacon, toast, eggs or sausage. Other popular topping alternatives include jam, peanut butter, nuts, fruit, honey, powdered sugar, whipped cream, cane syrup, and molasses.</p>
+        <h2>Common Ingridients</h2>
+        <p>A <code>ul</code> tag is a good example of why you should set good default <code>font-size</code> and <code>line-height</code> above the context of simply paragraph tags. Let any sort of article content get sizing from its parents.</p>
+        <ul>
+            <li>Eggs</li>
+            <li>Flour or other starchy base.</li>
+            <li>Pure deliciousness.</li>
+        </ul>  
+    </article>
+    </div>
+    <style jsx>{`
+        @import url('https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i')
+        * { box-sizing: border-box; }
+        holder {
+          font-family: 'Lato', sans-serif;
+          font-size: 16px;
+          line-height: 1.6;
+          color: rgba(0,0,0,.7);
+        }
+        .category {
+          text-transform: uppercase;
+          font-weight: 900;
+          font-size: 1.5rem;
+          color: rgba(0,0,0,.35);
+        }
+        .author-avatar img {
+          width: 30px;
+          margin: 0 .5rem;
+          border-radius: 15px;
+          position: relative;
+          top: .7rem;
+        }
+        .meta .author, .meta .pub-date { 
+          /*display: block;*/
+          font-weight: 900;
+          font-size: .7rem;
+          text-transform: uppercase;
+          color: rgba(0,0,0,.35);
+        }
+        .meta-border {
+          margin: 2rem 0;
+          width: 10rem;
+          height: .5rem;
+          background: rgba(0,0,0,.7);
+          display: block;
+        }
+        h1 { font-size: 3.5rem; font-weight: 900; margin: 1rem 0; line-height: 1.1;  }
+        h2 { font-size: 2.5rem; }
+        h3 { font-size: 2rem; }
+        p { font-size: 1.25rem; }
+        .article { max-width: 45rem; padding: 0 1rem; margin: 0 auto; }
+        img { max-width: 100%; }
+    `}</style>
+  </div>
+)
+
+export default Article
---
+++ pages/home.js
@@ -0,0 +1,296 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Main = () => (
+  <div>
+    <Head>
+        <title>FB TELUGU</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="theme-color" content="#000000" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta property="og:site_name" content="webcam.fbtelugu.com" />
+        <meta property="og:image" content="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg" name="image" />
+        <meta property="og:image:secure_url" content="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg" />
+        <meta property="og:image:width" content="600" />
+        <meta property="og:image:height" content="500" />
+        <meta property="og:image:alt" content="FB Telugu" />
+        <meta property="og:title" content="FB TELUGU" name="title" />
+        <meta content="news, trending, latest news" name="keywords" />
+        <meta property="og:description" content="New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers." name="description" />
+        <meta content="New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers." name="description" />
+        <meta property="og:url" content="https://webcam.fbtelugu.com/home" />
+        <meta property="og:type" content="website" />
+    </Head>
+    <Nav />
+
+    <div className='container'>
+        <div className='info'>
+            <h1>Article News Card</h1>
+        </div>
+        <div className='column'>
+            <div className='demo-title'>Normal</div>
+            <div className='post-module'>
+            <div className='thumbnail'>
+                <div className='date'>
+                <div className='day'>27</div>
+                <div className='month'>Mar</div>
+                </div>
+                <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg' />
+            </div>
+            <div className='post-content'>
+                <div className='category'>Photos</div>
+                <h1 className='title'>City Lights in New York</h1>
+                <h2 className='sub_title'>The city that never sleeps.</h2>
+                <p className='description'>New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                <div className='post-meta'>
+                <span className='timestamp'>
+                    <i className='fa fa-clock-o'></i>
+                    6 mins ago
+                </span>
+                <span className='comments'>
+                    <i className='fa fa-comments'></i>
+                    <a href='#'>39 comments</a>
+                </span>
+                </div>
+            </div>
+            </div>
+        </div>
+        <div className='column'>
+            <div className='demo-title'>Hover</div>
+            <div className='post-module hover'>
+            <div className='thumbnail'>
+                <div className='date'>
+                <div className='day'>27</div>
+                <div className='month'>Mar</div>
+                </div>
+                <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg' />
+            </div>
+            <div className='post-content'>
+                <div className='category'>Photos</div>
+                <h1 className='title'>City Lights in New York</h1>
+                <h2 className='sub_title'>The city that never sleeps.</h2>
+                <p className='description'>New York, the largest city in the U.S., is an architectural marvel with plenty of historic monuments, magnificent buildings and countless dazzling skyscrapers.</p>
+                <div className='post-meta'>
+                <span className='timestamp'>
+                    <i className='fa fa-clock-o'></i>
+                    6 mins ago
+                </span>
+                <span className='comments'>
+                    <i className='fa fa-comments'></i>
+                    <a href='#'>39 comments</a>
+                </span>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
+
+
+    <style jsx>{`
+      body {
+        background: #f2f2f2;
+        font-family: 'proxima-nova-soft', sans-serif;
+        font-size: 14px;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+      .post-module {
+        position: relative;
+        z-index: 1;
+        display: block;
+        background: #FFFFFF;
+        min-width: 270px;
+        height: 470px;
+        -webkit-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+        -moz-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+        box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+        -webkit-transition: all 0.3s linear 0s;
+        -moz-transition: all 0.3s linear 0s;
+        -ms-transition: all 0.3s linear 0s;
+        -o-transition: all 0.3s linear 0s;
+        transition: all 0.3s linear 0s;
+      }
+      .post-module:hover,
+      .hover {
+        -webkit-box-shadow: 0px 1px 35px 0px rgba(0, 0, 0, 0.3);
+        -moz-box-shadow: 0px 1px 35px 0px rgba(0, 0, 0, 0.3);
+        box-shadow: 0px 1px 35px 0px rgba(0, 0, 0, 0.3);
+      }
+      .post-module:hover .thumbnail img,
+      .hover .thumbnail img {
+        -webkit-transform: scale(1.1);
+        -moz-transform: scale(1.1);
+        transform: scale(1.1);
+        opacity: 0.6;
+      }
+      .post-module:hover .post-content .description,
+      .hover .post-content .description {
+        max-height: 100px;
+      }
+      .post-module .thumbnail {
+        background: #000000;
+        height: 400px;
+        overflow: hidden;
+      }
+      .post-module .thumbnail .date {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        z-index: 1;
+        background: #e74c3c;
+        width: 55px;
+        height: 55px;
+        padding: 12.5px 0;
+        -webkit-border-radius: 100%;
+        -moz-border-radius: 100%;
+        border-radius: 100%;
+        color: #FFFFFF;
+        font-weight: 700;
+        text-align: center;
+        -webkti-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+      }
+      .post-module .thumbnail .date .day {
+        font-size: 18px;
+      }
+      .post-module .thumbnail .date .month {
+        font-size: 12px;
+        text-transform: uppercase;
+      }
+      .post-module .thumbnail img {
+        display: block;
+        width: 120%;
+        -webkit-transition: all 0.3s linear 0s;
+        -moz-transition: all 0.3s linear 0s;
+        -ms-transition: all 0.3s linear 0s;
+        -o-transition: all 0.3s linear 0s;
+        transition: all 0.3s linear 0s;
+      }
+      .post-module .post-content {
+        position: absolute;
+        bottom: 0;
+        background: #FFFFFF;
+        width: 100%;
+        padding: 30px;
+        -webkti-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        -webkit-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        -moz-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        -ms-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        -o-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+      }
+      .post-module .post-content .category {
+        position: absolute;
+        top: -34px;
+        left: 0;
+        background: #e74c3c;
+        padding: 10px 15px;
+        color: #FFFFFF;
+        font-size: 14px;
+        font-weight: 600;
+        text-transform: uppercase;
+      }
+      .post-module .post-content .title {
+        margin: 0;
+        padding: 0 0 10px;
+        color: #333333;
+        font-size: 26px;
+        font-weight: 700;
+      }
+      .post-module .post-content .sub_title {
+        margin: 0;
+        padding: 0 0 20px;
+        color: #e74c3c;
+        font-size: 20px;
+        font-weight: 400;
+      }
+      .post-module .post-content .description {
+        display: block;
+        max-height: 0;
+        color: #666666;
+        font-size: 14px;
+        line-height: 1.8em;
+        -webkit-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        -moz-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        -ms-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        -o-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+        overflow: hidden;
+      }
+      .post-module .post-content .post-meta {
+        margin: 30px 0 0;
+        color: #999999;
+      }
+      .post-module .post-content .post-meta .timestamp {
+        margin: 0 16px 0 0;
+      }
+      .post-module .post-content .post-meta a {
+        color: #999999;
+        text-decoration: none;
+      }
+      .hover .post-content .description {
+        display: block !important;
+        max-height: 100px !important;
+        opacity: 1 !important;
+      }
+      .container {
+        max-width: 800px;
+        min-width: 640px;
+        margin: 0 auto;
+      }
+      .container:before,
+      .container:after {
+        content: '';
+        display: block;
+        clear: both;
+      }
+      .container .column {
+        width: 50%;
+        padding: 0 25px;
+        -webkti-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        float: left;
+      }
+      .container .column .demo-title {
+        margin: 0 0 15px;
+        color: #666666;
+        font-size: 18px;
+        font-weight: bold;
+        text-transform: uppercase;
+      }
+      .container .info {
+        width: 300px;
+        margin: 50px auto;
+        text-align: center;
+      }
+      .container .info h1 {
+        margin: 0 0 15px;
+        padding: 0;
+        font-size: 24px;
+        font-weight: 400;
+        color: #333333;
+      }
+      .container .info span {
+        color: #666666;
+        font-size: 12px;
+      }
+      .container .info span a {
+        color: #000000;
+        text-decoration: none;
+      }
+      .container .info span .fa {
+        color: #e74c3c;
+      }
+      
+    `}</style>
+  </div>
+)
+
+export default Main
